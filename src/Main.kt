921301ff1e02c7bad049bbfb620a8874e4c38dd0
Kostin52-02 <157310420+Kostin52-02@users.noreply.github.com>
2024-10-29 11:22:15 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Vvedite stroky:")
+    val input = readlnOrNull() ?: return
+    val result = StringBuilder()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    if (input.isNotEmpty()) {
+        var count = 1
+        for (i in 1..<input.length) {
+            if (input[i] == input[i - 1]) {
+                count++
+            } else {
+                result.append(input[i - 1])
+                if (count > 1) result.append(count)
+                count = 1
+            }
+        }
+        result.append(input[input.length - 1])
+        if (count > 1) result.append(count)
+
+        println(result.toString())
+    } else {
+        println("Stroka ne pustaya.")
     }
 }
